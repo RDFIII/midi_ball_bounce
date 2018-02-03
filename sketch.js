49baa7a646c3c647257e22c8391d8a7fd33a2025
RDF <foulkesr@gmail.com>
2018-02-03 10:46:50 -0500

    setup sketch file for basic functionality

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
+let ball;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	ball = new Ball(width/2, height/2, 20);
 }
 
 function draw() {
-
+	background(55);
+	ball.show();
+	ball.move();
 }
